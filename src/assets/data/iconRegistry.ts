5d59bbe1a3aabe961bd4f5367f6e3f21826daf8f
JohnAchicaizaG <159038108+JohnAchicaizaG@users.noreply.github.com>
2025-04-28 09:25:22 -0500

    secciones, herosection, responsive cards y  pagina

---
+++ src/assets/data/iconRegistry.ts
@@ -0,0 +1,61 @@
+// src/assets/data/iconRegistry.ts
+import iconAngular from "../icons/angular-min.svg";
+import iconAstro from "../icons/astro-min.svg";
+import iconNest from "../icons/nest-min.svg";
+import iconTailwind from "../icons/net-min.svg";
+import iconNodejs from "../icons/nodejs-min.svg";
+import iconPostgres from "../icons/postgresql-min.svg";
+import iconReact from "../icons/react-min.svg";
+import iconSpring from "../icons/springboot-min.svg";
+import iconSql from "../icons/sql-server-min.svg";
+import iconVue from "../icons/vue-min.svg";
+import iconGit from "../icons/git-min.svg";
+import iconHtml from "../icons/html-min.svg";
+import iconCss from "../icons/css-min.svg";
+import iconJs from "../icons/js-min.svg";
+import iconTs from "../icons/ts-min.svg";
+import iconAstroWhite from "../icons/astrowhite-min.svg";
+import iconFirebase from "../icons/firebase-min.svg";
+import iconNet from "../icons/net-min.svg";
+import esIcon from "../icons/es.svg";
+import enIcon from "../icons/en.svg";
+import modeIcon from "../icons/mode-theme.svg";
+
+import coverFastMeals from "../images/fastmeals-project/fastmeals-cover.webp";
+import coverInventory from "../images/inventory-project/inventory-cover.webp";
+import coverEepb from "../images/eepb-project/eepb-cover.webp";
+import coverSirenagest from "../images/sirenagest-project/sirenagest-cover.webp";
+
+// ✅ 1. Íconos SVG (componentes)
+export const iconRegistry: any = {
+    "mode-theme": modeIcon,
+    "es": esIcon,
+    "en": enIcon,
+    "angular": iconAngular,
+    "astro": iconAstro,
+    "astro-white": iconAstroWhite,
+    "firebase": iconFirebase,
+    "nest": iconNest,
+    "tailwind": iconTailwind,
+    "nodejs": iconNodejs,
+    "postgresql": iconPostgres,
+    "react": iconReact,
+    "springboot": iconSpring,
+    "sqlserver": iconSql,
+    "vue": iconVue,
+    "git": iconGit,
+    "html": iconHtml,
+    "css": iconCss,
+    "js": iconJs,
+    "ts": iconTs,
+    "net": iconNet,
+};
+
+// ✅ 2. Covers de proyectos (imágenes optimizadas)
+
+export const coverRegistry: any = {
+    "fastmeals-cover": coverFastMeals,
+    "inventory-cover": coverInventory,
+    "eepb-cover": coverEepb,
+    "sirenagest-cover": coverSirenagest,
+};
